ae58d5f53fc138543ca629b01bbb0aa5c8245536
LUGGGI(Lukas Beck) <st166506@stud.uni-stuttgart.de>
2022-12-16 20:08:30 +0100

    removed old code

--- updateContracts/main.py
+++ updateContracts/main.py
@@ -11,38 +11,29 @@ NEW_CONTRACT_DIR = "ContractsNew"
 SAVED_CONTRACT_DIR = "Contracts"
 SPREADSHEET = "Schließfächer_new.xlsx"
 
-contracts = []
 
 spreadsheet = UpdateSpreadsheet(SPREADSHEET)
 email = Email()
 
 filenames = listdir(NEW_CONTRACT_DIR)
 for filename in filenames:
-    if filename == "Formular_digital_sign.pdf" or filename == "Formular_normal_sign.pdf" or filename == "Nutzungsbedingungen.pdf":
+    if filename == "Formular_digital_sign.pdf" or filename == "Formular_normal_sign.pdf" or filename == "Nutzungsbedingungen.pdf" or filename == ".dummy":
         continue
     
     # read contract
     contract = Contract(NEW_CONTRACT_DIR + "/" + filename)
-    contracts.append(contract)
-
-    # add remaining values
-    # old_number_of_keys = spreadsheet.get_entry(contract.fields["number"])["keys"]
-    contract.add_remaining_values()
-
     print(contract.fields)
 
     updated = spreadsheet.update_entry(contract.fields)
 
-    if not updated:
-        continue
+    if updated:
+        save = input("Save entry to file? (y/N): ")
+        if save.lower() == "y":       
+            spreadsheet.workbook.save(spreadsheet.file)        
 
-    save = input("Save entry to file? (y/N): ")
-    if save.lower() == "y":       
-        spreadsheet.workbook.save(spreadsheet.file)        
-
-    send_email = input("Send Email? (y/N): ")
-    if send_email.lower() == "y": 
-        email.send_message(contract.fields["email"], NEW_CONTRACT_DIR, filename)
+        send_email = input("Send Email? (y/N): ")
+        if send_email.lower() == "y": 
+            email.send_message(contract.fields["email"], NEW_CONTRACT_DIR, filename)
 
     move_file = input("Move to Contracts folder? (Y/n): ") 
     if move_file.lower() != "n":       
